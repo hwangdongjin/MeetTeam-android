0e6f393753c6364840e9c1ddeeb3c87c36d87f35
Inyoung <wddjiy777@gmail.com>
2017-09-02 14:36:49 +0900

    맵 마커 로직 수정

--- MeetTeam/app/src/main/java/com/example/inyoung/teamapp/MapActivity.java
+++ MeetTeam/app/src/main/java/com/example/inyoung/teamapp/MapActivity.java
@@ -39,8 +39,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
     Button map_plusbutton;
     Button map_minusbutton;
 
-
-
     SharedPreferenceUtil sessDB;
     private NetworkService networkService;
     private ApplicationController application;
@@ -72,7 +70,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         map_findbutton = (Button) findViewById(R.id.mapfr_find);
         map_findbutton.setOnClickListener(this);
 
-
         go();
     }
 
@@ -80,7 +77,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         application = ApplicationController.getInstance();
         application.buildNetworkService();
         networkService = ApplicationController.getInstance().getNetworkService();
-
 
         sessDB = new SharedPreferenceUtil(getApplicationContext());
         Call<ResponseBody> thumbnail = networkService.post_mapshow(sessDB.getRoomTitle(),/*sessDB.getDate()*/year1+"-"+month1+"-"+dayOfMonth1);
@@ -98,18 +94,23 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
                         Log.i("mytag","reason:"+jsonObject.get("places").toString());
                     jsonArray = new JSONArray(jsonObject.get("places").toString());
                     for(int i=0;i<jsonArray.length();i++){
-                        JSONObject json = jsonArray.getJSONObject(i);
-                        JSONObject json2 = new JSONObject(json.toString());
-                        JSONObject json3 = new JSONObject(json2.get("loc").toString());
-                        JSONObject json4 = new JSONObject(json3.toString());
-                        JSONArray  jsonArray1 = new JSONArray(json4.get("coordinates").toString());
-                        for(int j=0;j<1;j++) {
-                            LatLng latLng = new LatLng(Double.parseDouble(jsonArray1.get(j + 1).toString()), Double.parseDouble(jsonArray1.get(j).toString()));
-                            MarkerOptions opt = new MarkerOptions();
-                            opt.position(latLng);
-                            opt.title((String) json2.get("userName"));
-                            mMap.addMarker(opt);
-                        }
+                        JSONObject place = jsonArray.getJSONObject(i);
+                        JSONObject loc = new JSONObject(place.get("loc").toString());
+                        JSONArray  coordinates = new JSONArray(loc.get("coordinates").toString());
+
+                        LatLng latLng = new LatLng(Double.parseDouble(coordinates.get(1).toString()), Double.parseDouble(coordinates.get(0).toString()));
+                        MarkerOptions opt = new MarkerOptions();
+                        opt.position(latLng);
+                        opt.title((String) place.get("userName"));
+                        mMap.addMarker(opt).showInfoWindow();
+
+//                        for(int j=0;j<1;j++) {
+//                            LatLng latLng = new LatLng(Double.parseDouble(coordinates.get(j + 1).toString()), Double.parseDouble(coordinates.get(j).toString()));
+//                            MarkerOptions opt = new MarkerOptions();
+//                            opt.position(latLng);
+//                            opt.title((String) place.get("userName"));
+//                            mMap.addMarker(opt).showInfoWindow();
+//                        }
                         }
                     }
                 } catch (JSONException e) {
@@ -135,11 +136,6 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(startingPoint,10));
         mMap.setOnMarkerClickListener(this);
         mMap.setOnMapLongClickListener(this);
-
-
-
-
-
     }
 
     @Override
@@ -162,37 +158,33 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
 
     @Override
     public boolean onMarkerClick(Marker marker) {
-        String text = "latitude ="+ marker.getPosition().latitude + "\n"+"longitude ="+ marker.getPosition().longitude;
-        Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
+        //String text = "latitude ="+ marker.getPosition().latitude + "\n"+"longitude ="+ marker.getPosition().longitude;
+        //String text = "위치가 등록되었습니다.";
+        //Toast.makeText(getApplicationContext(),text, Toast.LENGTH_LONG).show();
 
-        application = ApplicationController.getInstance();
-        application.buildNetworkService();
-        networkService = ApplicationController.getInstance().getNetworkService();
-
-        double longitude = marker.getPosition().longitude;
-        double latitude = marker.getPosition().latitude;
-
-        sessDB = new SharedPreferenceUtil(getApplicationContext());
-        Call<ResponseBody> thumbnalCall = networkService.post_map(sessDB.getSess(),sessDB.getRoomTitle(),/*sessDB.getDate()*/year1+"-"+month1+"-"+dayOfMonth1,longitude,latitude);
-        thumbnalCall.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
-                if(response.isSuccess()){
-
-
-
-                    go();
-                }
-
-            }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
-
-
+//        application = ApplicationController.getInstance();
+//        application.buildNetworkService();
+//        networkService = ApplicationController.getInstance().getNetworkService();
+//
+//        double longitude = marker.getPosition().longitude;
+//        double latitude = marker.getPosition().latitude;
+//
+//        sessDB = new SharedPreferenceUtil(getApplicationContext());
+//        Call<ResponseBody> thumbnalCall = networkService.post_map(sessDB.getSess(),sessDB.getRoomTitle(),/*sessDB.getDate()*/year1+"-"+month1+"-"+dayOfMonth1,longitude,latitude);
+//        thumbnalCall.enqueue(new Callback<ResponseBody>() {
+//            @Override
+//            public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
+//                if(response.isSuccess()){
+//                    go();
+//                }
+//
+//            }
+//
+//            @Override
+//            public void onFailure(Throwable t) {
+//
+//            }
+//        });
         return false;
     }
     @Override
@@ -202,9 +194,28 @@ public class MapActivity extends AppCompatActivity implements OnMapReadyCallback
         markerOptions.position(latLng);
         mMap.animateCamera(CameraUpdateFactory.newLatLng(latLng));
         mMap.addMarker(markerOptions);
-        Toast.makeText(getApplicationContext(),"위치를 확정하시려면 마커를 한번 더 클릭해주세요",Toast.LENGTH_LONG).show();
 
+        application = ApplicationController.getInstance();
+        application.buildNetworkService();
+        networkService = ApplicationController.getInstance().getNetworkService();
 
+        double longitude = latLng.longitude;
+        double latitude = latLng.latitude;
 
+        sessDB = new SharedPreferenceUtil(getApplicationContext());
+        Call<ResponseBody> thumbnalCall = networkService.post_map(sessDB.getSess(),sessDB.getRoomTitle(),/*sessDB.getDate()*/year1+"-"+month1+"-"+dayOfMonth1,longitude,latitude);
+        thumbnalCall.enqueue(new Callback<ResponseBody>() {
+            @Override
+            public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
+                if(response.isSuccess()){
+                    go();
+                }
+            }
+
+            @Override
+            public void onFailure(Throwable t) {
+
+            }
+        });
     }
 }
