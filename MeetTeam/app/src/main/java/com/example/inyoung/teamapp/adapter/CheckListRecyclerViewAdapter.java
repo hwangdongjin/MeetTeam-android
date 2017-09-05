04a1bf740929c3ffbc5ae0c531dfd05148dce5f2
hwangdongjin <sosa1991@naver.com>
2017-09-05 21:23:20 +0900

    clist통신 완료/프로그레스바

--- MeetTeam/app/src/main/java/com/example/inyoung/teamapp/adapter/CheckListRecyclerViewAdapter.java
+++ MeetTeam/app/src/main/java/com/example/inyoung/teamapp/adapter/CheckListRecyclerViewAdapter.java
@@ -22,11 +22,6 @@ import com.example.inyoung.teamapp.dto.CheckAddDTO;
 import com.example.inyoung.teamapp.dto.CheckListDTO;
 import com.squareup.okhttp.ResponseBody;
 
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
 import java.util.ArrayList;
 
 import retrofit.Call;
@@ -43,6 +38,7 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
     private ArrayList<CheckAddDTO> checkAddList, checkShowList;
     private Context context;
     private AlertDialog dlg;
+    int size;
     CheckListAddRecyclerViewAdapter roAdapter;
     private NetworkService networkService;
     ApplicationController application;
@@ -54,36 +50,29 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
     static int pro;
     String st_name, st_do;
     Button btn_ok, btn_cancel;
-    public JSONObject jsonObject;
-    public JSONArray jsonArray;
-    public JSONArray clistArray;
-    public JSONObject clistObject;
-    public CheckListRecyclerViewAdapter(ArrayList<CheckListDTO> checkList,Context context) {
+    public CheckListRecyclerViewAdapter(ArrayList<CheckListDTO> checkList, ArrayList<CheckAddDTO> checkAddList, int size, Context context) {
         this.checkList = checkList;
+        this.checkAddList = checkAddList;
         this.context = context;
-
+        this.size = size;
     }
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private TextView checkRoom_name;
         private Button manager_Button;
         private ProgressBar progressBar;
-        private RecyclerView checkListView;
         private TextView progressNum;
-        private TextView manage_Name, manage_Do;
         private Button btn_up, btn_down;
+        private RecyclerView checkListView;
 
         public ViewHolder(View itemView) {
             super(itemView);
             checkRoom_name = (TextView) itemView.findViewById(R.id.CheckRoomName);
             manager_Button = (Button) itemView.findViewById(R.id.manager_button);
-            manage_Name = (TextView) itemView.findViewById(R.id.manager_Name11);
-            manage_Do = (TextView) itemView.findViewById(R.id.manager_do11);
             progressBar = (ProgressBar) itemView.findViewById(R.id.progressBar);
-            checkListView = (RecyclerView) itemView.findViewById(R.id.CheckListView);
             progressNum = (TextView) itemView.findViewById(R.id.progressNum);
             btn_up = (Button) itemView.findViewById(R.id.btn_up);
             btn_down = (Button) itemView.findViewById(R.id.btn_down);
-
+            checkListView = (RecyclerView) itemView.findViewById(R.id.clistRecyclerView);
         }
     }
 
@@ -91,10 +80,8 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
     public CheckListRecyclerViewAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_check_list, parent, false);
         CheckListRecyclerViewAdapter.ViewHolder vholder = new CheckListRecyclerViewAdapter.ViewHolder(itemView);
-        initClistShow(vholder,viewType);
         return vholder;
     }
-
     @Override
     public void onBindViewHolder(final CheckListRecyclerViewAdapter.ViewHolder holder, final int position) {
         sessDB = new SharedPreferenceUtil(context);
@@ -103,6 +90,42 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
         networkService = ApplicationController.getInstance().getNetworkService();
         holder.checkRoom_name.setText(checkList.get(position).getCheck_RoomName());
         Log.i("mytag", "position:" + position);
+        switch (position) {
+            case 0:
+                 initClistShow(holder,position);
+                break;
+            case 1:
+                 initClistShow(holder,position);
+                break;
+            case 2:
+                initClistShow(holder,position);
+                break;
+            case 3:
+                initClistShow(holder,position);
+                break;
+            case 4:
+                initClistShow(holder,position);
+                break;
+            case 5:
+                initClistShow(holder,position);
+                break;
+            case 6:
+                initClistShow(holder,position);
+                break;
+            case 7:
+                initClistShow(holder,position);
+                break;
+            case 8:
+                initClistShow(holder,position);
+                break;
+            case 9:
+                initClistShow(holder,position);
+                break;
+            case 10:
+                initClistShow(holder,position);
+                break;
+        }
+
         holder.btn_up.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -111,7 +134,7 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
         holder.manager_Button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(final View v) {
-                LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+                final LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
                 final View view = inflater.inflate(R.layout.item_check_list_member2, null, false);
                 dlg = new AlertDialog.Builder(context).create();
                 dlg.setView(view);
@@ -122,7 +145,7 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
                 btn_cancel = (Button) view.findViewById(R.id.btn_cancel);
                 btn_ok.setOnClickListener(new View.OnClickListener() {
                     @Override
-                    public void onClick(View v) {
+                    public void onClick(final View v) {
                         if ("".equals(edt_name.getText().toString())) {
                             Toast.makeText(context, "팀원을 입력해주세요", Toast.LENGTH_LONG).show();
                         } else {
@@ -140,6 +163,7 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
                                 public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
                                     if (response.isSuccess()) {
                                         Toast.makeText(context, "성공하였습니다", Toast.LENGTH_LONG).show();
+                                        
                                         if (progressNum != 0) {
                                             holder.progressBar.incrementProgressBy(pro);
                                             holder.progressNum.setText(String.valueOf(pro));
@@ -192,58 +216,22 @@ public class CheckListRecyclerViewAdapter extends RecyclerView.Adapter<CheckList
     }*/
 
 
-   public void initClistShow(final CheckListRecyclerViewAdapter.ViewHolder holder, final int position) {
-        sessDB = new SharedPreferenceUtil(context);
-        application = ApplicationController.getInstance();
-        application.buildNetworkService();
-        networkService = ApplicationController.getInstance().getNetworkService();
-        Call<ResponseBody> call = networkService.post_taskShow(sessDB.getRoomTitle());
-        call.enqueue(new Callback<ResponseBody>() {
-            @Override
-            public void onResponse(Response<ResponseBody> response, Retrofit retrofit) {
-                if (response.isSuccess()) {
-                    try {
-                        jsonArray = new JSONArray(response.body().string());
-                        checkAddList = new ArrayList<>();
-                        Log.i("mytag","jsonleng:"+jsonArray.length());
-                            JSONObject jsonObject2 = jsonArray.getJSONObject(position);
-                            JSONArray clistArray2 = new JSONArray(jsonObject2.get("clist").toString());
-                            for (int j = 0; j < clistArray2.length(); j++) {
-                                JSONObject clistObject2 = clistArray2.getJSONObject(j);
-                                checkAddList.add(new CheckAddDTO((String) clistObject2.get("name"), (String) clistObject2.get("list"), (Boolean) clistObject2.get("isCheck"),clistArray2.length()));
-                                Log.i("mytag", "length22:" + ((String) clistObject2.get("name")));
-                                Log.i("mytag", "length22:" + ((String) clistObject2.get("list")));
-                            }
+    public void initClistShow(CheckListRecyclerViewAdapter.ViewHolder holder, final int position) {
 
-                        for(int k=0;k<checkAddList.size();k++){
-                            Log.i("mytag","clistarr:"+checkAddList.get(k).getClistNum());
-                        }
-                        holder.checkListView.setHasFixedSize(false);
-                        holder.checkListView.setLayoutManager(new LinearLayoutManager(context,LinearLayoutManager.VERTICAL,false));
-                        roAdapter = new CheckListAddRecyclerViewAdapter(checkAddList,context);
-                        roAdapter.notifyItemInserted(0);
-                        roAdapter.notifyDataSetChanged();
-                        holder.checkListView.setAdapter(roAdapter);
-                        progressNum = roAdapter.getCheckNum();
-                        total = roAdapter.getItemCount();
-                        finish = (double) progressNum / total;
-                        pro = (int) (100 * finish);
-
-
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
+        checkShowList = new ArrayList<>();
+        for (int i = 0; i < checkAddList.size(); i++) {
+            if (checkAddList.get(i).getClistNum() == position) {
+                checkShowList.add(new CheckAddDTO(checkAddList.get(i).getManager_Name(), checkAddList.get(i).getManager_Do(), false, checkAddList.get(i).getClistNum()));
             }
-
-            @Override
-            public void onFailure(Throwable t) {
-
-            }
-        });
+        }
+        holder.checkListView.setHasFixedSize(false);
+        holder.checkListView.setLayoutManager(new LinearLayoutManager(context, LinearLayoutManager.VERTICAL, false));
+        roAdapter = new CheckListAddRecyclerViewAdapter(checkShowList, context);
+        roAdapter.notifyDataSetChanged();
+        holder.checkListView.setAdapter(roAdapter);
+        progressNum = roAdapter.getCheckNum();
+        total = roAdapter.getItemCount();
+        finish = (double) progressNum / total;
+        pro = (int) (100 * finish);
     }
 }
-
